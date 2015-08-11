16e00837df118eb0c62fb08d659bbc039bbab193
Chris Brackert <cbrackert@gmail.com>
2015-08-10 18:12:49 -0700

    Create block and unit tests

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 CategorizeSignals
 =================
 
-What does this block do?
+Assign categories to posts based on a regular expression search
 
 Properties
 ----------
-None
+-   **Attribute to Set**: Attribute to set *Category to Assign* to on Signal.
+-   **Match String**: String to evaluate each regex pattern against.
+-   **Categories**: List of categories and regex patterns.
+    -   **Category to Assign**: Category to append to *Attribute to Set* if there is a match.
+    -   **Regular Expression Patterns**: List of regular expression patterns. The category is considered a match if any of these patterns are found in the *Match String*.
 
 Dependencies
 ------------
@@ -21,4 +25,6 @@ Any list of signals.
 
 Output
 ------
-Same list of signals as input.
+Same list of signals as input with the added attribute *Attribute to Set* as defined in the configuration. *Attribute to Set* is a list with a category appended to it for each matching *Category* with a corresponding matching *Regular Expression Pattern*.
+
+If *Attribute to Set* already exists on the input signal and it is not a list then the blocks logs an error and the signal is not modified.
--- categorize_signals_block.py
+++ categorize_signals_block.py
@@ -1,16 +1,61 @@
+import re
 from nio.common.block.base import Block
 from nio.common.discovery import Discoverable, DiscoverableType
-from nio.metadata.properties import VersionProperty
+from nio.metadata.properties import StringProperty, ExpressionProperty, \
+    VersionProperty, ObjectProperty, PropertyHolder, ListProperty
+
+
+class Category(PropertyHolder):
+    category = StringProperty(default='cat', title='Category to Assign')
+    patterns = ListProperty(str, title='Regular Expression Patterns')
 
 
 @Discoverable(DiscoverableType.block)
 class CategorizeSignals(Block):
 
-    """ This is the Example block. Put a brief description here. """
+    """Assign categories to posts based on a regular expression search"""
 
     version = VersionProperty('0.1.0')
+    attr = StringProperty(title='Attribute to set', default='cats')
+    string = ExpressionProperty(title="Match String",
+                                default='', attr_default=Exception)
+    categories = ListProperty(Category, title='Categories')
+
+    def __init__(self):
+        super().__init__()
+        self._compiled = None
+
+    def configure(self, context):
+        super().configure(context)
+        self._compiled = self._compile_patterns()
 
     def process_signals(self, signals, input_id='default'):
         for signal in signals:
-            pass
+            self._process_signal(signal)
         self.notify_signals(signals, output_id='default')
+
+    def _compile_patterns(self):
+        """Build and compile regex patterns.
+
+        Return:
+            list of tuples ((category, compiled_regex_pattern))
+
+        """
+        return_list = []
+        for category in self.categories:
+            return_list.append((category.category,
+                                re.compile('|'.join(category.patterns))))
+        return return_list
+
+    def _process_signal(self, signal):
+        for category in self._compiled:
+            # Prepare signal by checking the caetgory attribute
+            if not hasattr(signal, self.attr):
+                setattr(signal, self.attr, [])
+            if not isinstance(getattr(signal, self.attr), list):
+                self._logger.exception('Signal category attribute is not list')
+                return
+            # Check if regex matchces and append category to attr if match
+            match = category[1].search(str(self.string(signal)))
+            if match is not None:
+                getattr(signal, self.attr).append(category[0])
